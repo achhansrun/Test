653936f030c1385ed8609e00697a8636e02e9c86
Adrian Antonio <adrianvantonio@yahoo.com>
2023-05-24 18:22:28 -0800

    Version Change 5.1 Switch engine version

---
+++ TEST/Intermediate/Build/Win64/TESTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.34.31937
+#include "C:/Users/Fals3/Desktop/Summer Repo/Test/TEST/Intermediate/Build/Win64/TESTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_0.h"
---
+++ TEST/Intermediate/Build/Win64/UnrealEditor/Inc/TEST/UHT/TESTGameMode.generated.h
@@ -0,0 +1,96 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "TESTGameMode.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TEST_TESTGameMode_generated_h
+#error "TESTGameMode.generated.h already included, missing '#pragma once' in TESTGameMode.h"
+#endif
+#define TEST_TESTGameMode_generated_h
+
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_SPARSE_DATA
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_RPC_WRAPPERS
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_ACCESSORS
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATESTGameMode(); \
+	friend struct Z_Construct_UClass_ATESTGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ATESTGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TEST"), TEST_API) \
+	DECLARE_SERIALIZER(ATESTGameMode)
+
+
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesATESTGameMode(); \
+	friend struct Z_Construct_UClass_ATESTGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ATESTGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TEST"), TEST_API) \
+	DECLARE_SERIALIZER(ATESTGameMode)
+
+
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	TEST_API ATESTGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATESTGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(TEST_API, ATESTGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATESTGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	TEST_API ATESTGameMode(ATESTGameMode&&); \
+	TEST_API ATESTGameMode(const ATESTGameMode&); \
+public: \
+	TEST_API virtual ~ATESTGameMode();
+
+
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	TEST_API ATESTGameMode(ATESTGameMode&&); \
+	TEST_API ATESTGameMode(const ATESTGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(TEST_API, ATESTGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATESTGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATESTGameMode) \
+	TEST_API virtual ~ATESTGameMode();
+
+
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_9_PROLOG
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_SPARSE_DATA \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_RPC_WRAPPERS \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_ACCESSORS \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_INCLASS \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_SPARSE_DATA \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_ACCESSORS \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TEST_API UClass* StaticClass<class ATESTGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_Fals3_Desktop_Summer_Repo_Test_TEST_Source_TEST_TESTGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
